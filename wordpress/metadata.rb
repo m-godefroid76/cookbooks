074d50fb4f95e2a69cd876fd493d9f9695a0274b
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-01-27 14:39:40 +0000

    swap

---
+++ wordpress/metadata.rb
@@ -0,0 +1,9 @@
+name        "deploy-cookbook"
+description 'Deployment cookbook for Restofactory'
+maintainer  "Restofactory"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends "nginx"
+depends "apache2"
+depends "swap"
---
+++ wordpress/recipes/swap.rb
@@ -0,0 +1,3 @@
+swap_file '/mnt/swap' do
+  size      8192    # MBs
+end
