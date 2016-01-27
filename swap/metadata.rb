64ba308625e643ad446170113ca61b8ad8c140d2
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-01-27 16:13:22 +0000

    swap

---
+++ swap/metadata.rb
@@ -0,0 +1,7 @@
+name             'swap'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures swap'
+long_description 'Installs/Configures swap'
+version          '0.1.0'
