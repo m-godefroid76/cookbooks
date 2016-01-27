8c17af9c2ad56eefea5736761467c60aa67d8c4d
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-01-27 12:38:06 +0000

    first commit

---
+++ deploy-cookbook/metadata.rb
@@ -0,0 +1,8 @@
+name        "deploy-cookbook"
+description 'Deployment cookbook for GraphyStories Backend'
+maintainer  "GraphyStories"
+license     "Apache 2.0"
+version     "1.0.0"
+
+depends "swap"
+depends "deploy"
