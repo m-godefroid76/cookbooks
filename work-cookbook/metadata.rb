e731c0953c18baa3f5344e0d2bab567c63aa2530
root <root@ip-172-31-13-143.eu-west-1.compute.internal>
2016-01-27 09:45:16 +0000

    first commit

--- work-cookbook/metadata.rb
+++
@@ -1,9 +0,0 @@
-name        "work-cookbook"
-description 'Working cookbook for RestoFactory Developers'
-maintainer  "RestoFactory"
-license     "Apache 2.0"
-version     "1.0.0"
-
-#depends "git"
-depends "apache2"
-#depends "mysql"
--- work-cookbook/recipes/install.rb
+++
@@ -1,1 +0,0 @@
-include_recipe 'work-cookbook::apache'
