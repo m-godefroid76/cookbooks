45bf5f2247b8f00f5f171f969bfda7419701eb20
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-01-27 13:02:35 +0000

    first commit

--- deploy-cookbook/metadata.rb
+++
@@ -1,8 +0,0 @@
-name        "deploy-cookbook"
-description 'Deployment cookbook for GraphyStories Backend'
-maintainer  "GraphyStories"
-license     "Apache 2.0"
-version     "1.0.0"
-
-depends "swap"
-depends "deploy"
--- deploy-cookbook/recipes/swap.rb
+++
@@ -1,3 +0,0 @@
-swap_file '/mnt/swap/swp' do
-  size      2048    # MBs
-end
