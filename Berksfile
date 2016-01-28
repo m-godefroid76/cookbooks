9733637fa1ed8faec5e9e1255d7294c6baff642b
root <root@ip-172-31-46-167.eu-west-1.compute.internal>
2016-01-28 14:59:00 +0000

    first commit

--- Berksfile
+++
@@ -1,8 +0,0 @@
-source "https://supermarket.chef.io"
-
-metadata
-
-cookbook 'apache2'
-cookbook 'php'
-cookbook 'mysql'
-cookbook 'swap'
--- metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'cookbook'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures cookbook'
-long_description 'Installs/Configures cookbook'
-version          '0.1.0'
-
-depends 'apache2'
