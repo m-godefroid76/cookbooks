1393aa40dc694ab3d603caabbfc995c581c3e5f8
m-godefroid76 <m.godefroid76@gmail.com>
2016-01-28 12:25:36 +0000

    apache

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,26 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+# Install Apache and configure its service.
+include_recipe 'apache2::default'
+
+# Create and enable our custom site.
+web_app 'frontend' do
+  template 'scalable.conf.erb'
+end
+
+# disable default site
+apache_site '000-default' do
+  enable false
+end
+
+# enable frontend site
+apache_site 'frontend' do
+  enable true
+end
+
+# restart apache2
+service "apache2" do
+    action [ :restart ]
+end
