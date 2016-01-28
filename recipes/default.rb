d5fff9257915f580aad388d6963f5b8a7aa70225
root <root@ip-172-31-46-167.eu-west-1.compute.internal>
2016-01-28 15:26:59 +0000

    first commit

---
+++ recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: learn_chef_apache2
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package 'apache2'
+
+service 'apache2' do
+  supports :status => true
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
