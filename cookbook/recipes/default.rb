04fe332d9199b1e874f2403951be33f88b8a8047
root <root@ip-172-31-46-167.eu-west-1.compute.internal>
2016-01-28 15:46:50 +0000

    test

--- cookbook/recipes/default.rb
+++ cookbook/recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
+
+package 'apache2'
+
+service 'apache2' do
+  supports :status => true
+  action [:enable, :start]
+end
+
+template '/var/www/html/index.html' do
+  source 'index.html.erb'
+end
