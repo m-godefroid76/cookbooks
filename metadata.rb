c4ebde3a628f4e18c1028e94532e591d65a881aa
m-godefroid76 <m.godefroid76@gmail.com>
2016-01-28 12:31:48 +0000

    first commit

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures cookbook'
 long_description 'Installs/Configures cookbook'
 version          '0.1.0'
+
+depends 'apache2'
