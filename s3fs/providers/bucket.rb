8350f48743b706455c5756649dc8652ba11884ff
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-01-29 16:38:16 +0000

    s3fs new v

---
+++ s3fs/providers/bucket.rb
@@ -0,0 +1,28 @@
+action :mount do
+    
+  if @new_resource.mount_folder != nil
+    
+    folder = @new_resource.mount_folder
+    
+    directory "/mnt/#{folder}" do
+      mode "0755"
+      owner "root"
+      group "root"
+      action :create
+      recursive true
+    end
+    
+    bucket = @new_resource.bucket_name
+    
+    bash "mount s3 bucket on folder" do
+      code <<-EOH
+      sudo bash -c 'export AWSACCESSKEYID=#{ node[:access_key] }; export AWSSECRETACCESSKEY=#{ node[:secret_key] }; s3fs #{bucket} /mnt/#{folder} -o allow_other'
+      EOH
+    
+## a instrucao not_if parece nao funcionar devidamente  quando chamada varias vezes seguidas num loop      
+      not_if { ::File.directory?("/mnt/#{folder}/mysql/") }
+    end
+  end
+
+
+end
