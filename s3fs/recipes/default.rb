f03bdb59ae3da16b54e3f3a54aa20a6fef601f25
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-02-01 11:25:33 +0000

    test

--- s3fs/recipes/default.rb
+++ s3fs/recipes/default.rb
@@ -40,3 +40,15 @@ bash "install s3fs" do
 
   not_if { File.exists?("/usr/bin/s3fs") }
 end
+
+directory "/var/www/html/test" do
+  mode "0755"
+  owner "root"
+  group "root"
+  action :create
+  recursive true
+end
+
+file '/etc/fuse.conf' do
+  content  "user_allow_other"
+end
