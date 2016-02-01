88b8a1d5b07bb220865559f71eab1f86ee57f8cc
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-02-01 09:27:07 +0000

    patch fuse-utils

--- s3fs/recipes/default.rb
+++ s3fs/recipes/default.rb
@@ -1,4 +1,4 @@
-%w{ build-essential pkg-config libcurl4-openssl-dev libfuse-dev fuse-utils libfuse2 libxml2-dev mime-support }.each do |pkg|
+%w{ build-essential pkg-config libcurl4-openssl-dev libfuse-dev libfuse2 libxml2-dev mime-support }.each do |pkg|
   package pkg
 end
 
