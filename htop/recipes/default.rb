6a040ee7b136df71bc848ee1e912000fd9099595
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-02-03 11:33:50 +0000

    mv wpconfig

--- htop/recipes/default.rb
+++ htop/recipes/default.rb
@@ -10,3 +10,13 @@ when "ubuntu","debian"
     action :install
   end
 end
+
+template '/mnt/srv/www/wordpress/current/wp-config.php' do
+  source 'wp-config.php.erb'
+  variables({
+     :DB_NAME => node[:DB_NAME],
+     :DB_HOST => node[:DB_HOST],
+     :DB_USER => node[:DB_USER],
+     :DB_PASSWORD => node[:DB_PASSWORD]
+  })
+end
