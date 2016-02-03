35a9582b4c124f9c4b799a2b9450fb4a07ff0d4a
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-02-03 14:33:47 +0000

    fix

--- htop/recipes/default.rb
+++ htop/recipes/default.rb
@@ -1,7 +1,7 @@
 #
 # Cookbook Name:: htop
 # Recipe:: default
-#
+# encoding: utf-8
 #
 
 case node[:platform] 
@@ -13,10 +13,4 @@ end
 
 template '/mnt/srv/www/wordpress/current/wp-config.php' do
   source 'wp-config.php.erb'
-  variables({
-     :DB_NAME => node[:DB_NAME],
-     :DB_HOST => node[:DB_HOST],
-     :DB_USER => node[:DB_USER],
-     :DB_PASSWORD => node[:DB_PASSWORD]
-  })
 end
