6a678f7322b61964a66ee06f57a68c72d9f0949e
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-02-09 11:10:47 +0000

    fix login

--- login/recipes/default.rb
+++ login/recipes/default.rb
@@ -1,3 +1,10 @@
+directory '/mnt/srv/www/wordpress/current/login' do
+  owner 'root'
+  group 'root'
+  mode '0755'
+  action :create
+end
+
 template '/mnt/srv/www/wordpress/current/login/index.php' do
   source 'index.php.erb'
   owner 'root'
