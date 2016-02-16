8488b244db0407b62055f1bc1dfea3487be33a73
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-02-16 10:14:57 +0000

    feat unmount s3 on shutdown

--- shutdown/recipes/default.rb
+++ shutdown/recipes/default.rb
@@ -9,3 +9,9 @@ bash "logrotate on shutdown" do
   command 'sudo /usr/sbin/logrotate -f /etc/logrotate.conf'
   action :nothing
 end
+
+bash "unmount s3fs" do
+  user 'root'
+  command 'sudo umount /srv/www/wordpress/current/wp-content/uploads/'
+  action :nothing
+end
