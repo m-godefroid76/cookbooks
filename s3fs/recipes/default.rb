a89c68a77f6a33f57709e0b12c4bcf43b2a274e4
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-03-02 13:06:13 +0000

    fix cache s3fs

--- s3fs/recipes/default.rb
+++ s3fs/recipes/default.rb
@@ -64,5 +64,5 @@ end
 
 execute 'mount uploads folder' do
   user "root"
-  command "sudo s3fs #{ node[:bucket] } -o allow_other /mnt/uploads"
+  command "sudo s3fs #{ node[:bucket] } -o allow_other,use_cache=/tmp/cache /mnt/uploads"
 end
