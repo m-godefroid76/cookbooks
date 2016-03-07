cc9910e2e5b3d91cea6e6a1f1719f1857704496f
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-03-07 13:38:33 +0000

    stop cron

--- crontab/recipes/default.rb
+++ crontab/recipes/default.rb
@@ -1,8 +1,8 @@
-cron "wp-cron" do
-    action :create
-    minute "*/10"
-    command '/usr/bin/php /srv/www/wordpress/current/wp-cron-mu.php > /var/log/cron.log 1>&1'
-end
+# cron "wp-cron" do
+    # action :create
+    # minute "*/10"
+    # command '/usr/bin/php /srv/www/wordpress/current/wp-cron-mu.php > /var/log/cron.log 1>&1'
+# end
 
 cron "logrotate" do
     action :create
