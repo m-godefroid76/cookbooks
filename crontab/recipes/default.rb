7434ec3cc1eb68da5b40eb94be70af13381d5ec1
m-godefroid76 <michael.godefroid@opengraphy.com>
2016-06-21 14:23:50 +0000

    fix backup db cron

--- crontab/recipes/default.rb
+++ crontab/recipes/default.rb
@@ -7,6 +7,14 @@ end
 cron "logrotate" do
     action :create
     minute "0"
+    hour '3'
     command '/usr/sbin/logrotate /etc/logrotate.conf > /var/log/cron.log 1>&1'
 end
 
+cron "backup-db" do
+    action :create
+    minute '0'
+    hour '2'
+    weekday "1"
+    command '/scripts/backup-db.sh > /var/log/cron.log 1>&1'
+end
